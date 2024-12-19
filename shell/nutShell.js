7dff540cc2683e366fc1fb79e10d91617edc87f3
devdatta <183721457+devdattadhage@users.noreply.github.com>
2024-12-19 18:22:19 +0530

    second commit

--- shell/nutShell.js
+++ shell/nutShell.js
@@ -1,5 +1,34 @@
-while (true) {
-  const userCommand = prompt('nutshell ~ %');
+const rootDirectory = 'nutshell ~ %';
+let currDirectory = rootDirectory;
 
-  console.log(userCommand);
+const directory = function() {
+  currentDirectory = 
 }
+
+const runCd = function (args) {
+  currDirectory = args.join();
+  return;
+}
+
+const runCommand = function (userCommand) {
+  const [command, ...args] = userCommand.split(' ');
+
+  switch (command) {
+    case 'echo': return args.join();
+    case 'cd': return runCd(args);
+
+    default: return 'zsh: command not found: ' + command;
+  }
+}
+
+const shell = function () {
+  while (true) {
+    const userCommand = prompt(currDirectory);
+
+    const commandOutput = runCommand(userCommand);
+
+    console.log(commandOutput);
+  }
+}
+
+shell();
